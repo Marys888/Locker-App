6f10b446e7cf1114f6799555730ebbca7dbe0f82
Marys888 <severovamaha1999@gmail.com>
2025-05-25 16:11:05 +0200

    Update app entry point to start the program

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,13 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        int[] data = new DataRepository().getData();
+        DataHandler handler = new DataHandler();
 
+        for(int num : data){
+            System.out.println("Initial value is " + num);
+            int newNum = handler.modify(num);
+            System.out.println("New value is " + newNum);
+        }
     }
 }
